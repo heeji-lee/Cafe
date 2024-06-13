c814f689730de7494f375ef4b4db8f96a281f992
user <vvlddiz@gmail.com>
2024-06-13 18:20:47 +0900

    CafeDTO 회원가입관련 코드 추가

--- src/main/java/com/project/cafe/dto/CafeDTO.java
+++ src/main/java/com/project/cafe/dto/CafeDTO.java
@@ -1,4 +1,9 @@
 package com.project.cafe.dto;
 
 public class CafeDTO {
+    private String userId;
+    private String password;
+    private String gender;
+    private String phone;
+    private String birthday;
 }
