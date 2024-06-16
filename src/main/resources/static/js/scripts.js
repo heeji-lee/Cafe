0febdf0901df77a0dc8f9edde26cd36ede7a2d53
heeji-lee <dpgbanjdal@gmail.com>
2024-06-16 22:55:17 +0900

    헤더 기초 작업

---
+++ src/main/resources/static/js/scripts.js
@@ -0,0 +1,19 @@
+$(function(){
+    var gnb_height = '100px';  // 1단계 메뉴 높이
+    var gnb_dp2_height = '180px'; // 2단계 메뉴 높이
+    var $gnb = $('#gnb');
+
+    // gnb 초기화(2단계 메뉴 숨기기)
+    $gnb.css('height', gnb_height);
+
+    // 마우스 hover 드롭다운 메뉴
+    $gnb.on({
+        mouseenter: function(){
+            $(this).stop().animate({ height: gnb_dp2_height });
+        },
+        mouseleave: function(){
+            $(this).stop().animate({ height: gnb_height });
+        }
+    });
+
+});
